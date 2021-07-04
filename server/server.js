7ab08ea686de27637bc183b6c37e943a0a7357fb
Riley Martin <riley.willow@gmail.com>
2021-07-04 17:10:39 -0600

    Follow tutorial for building schema and route in order to be able to query and mutate data at a single endpoint

--- server/server.js
+++ server/server.js
@@ -5,14 +5,114 @@ const { buildSchema } = require('graphql')
 //construct a schema, using graphql schema lang
 
 const schema = buildSchema(`
-    type Query {
-      hello: String
-    }`);
+  input MessageInput {
+    content: String
+    author: String
+  }
+
+  type Message {
+    id: ID!
+    content: String
+    author: String
+  }
+
+  type RandomDie {
+    numSides: Int!
+    rollOnce: Int!
+    roll(numRolls: Int!): [Int]
+  }
+
+  type Mutation {
+    createMessage(input: MessageInput): Message
+    updateMessage(id: ID!, input: MessageInput): Message
+  }
+
+  type Query {
+    hello: String
+    goodbye: String
+    rollDice(numDice: Int!, numSides: Int): [Int]
+    quoteOfTheDay: String
+    random: Float!
+    rollThreeDice: [Int]
+    getDie(numSides: Int): RandomDie
+    getMessage(id: ID!): Message
+  }`);
+
+class Message {
+  constructor(id, {content, author}) {
+    this.id = id;
+    this.content = content;
+    this.author = author;
+  }
+}
+
+// This class implements the RandomDie GraphQL type
+class RandomDie {
+  constructor(numSides) {
+    this.numSides = numSides;
+  }
+
+  rollOnce() {
+    return 1 + Math.floor(Math.random() * this.numSides);
+  }
+
+  roll({numRolls}) {
+    var output = [];
+    for (var i = 0; i < numRolls; i++) {
+      output.push(this.rollOnce());
+    }
+    return output;
+  }
+}
 
 //the root provides a resolver function for each API endpoint
+let fakeDatabase = {};
 const root = {
   hello: () => {
     return 'Hello world!';
+  },
+  goodbye: () => {
+    return 'Fuck off!'
+  },
+  quoteOfTheDay: () => {
+    return Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within';
+  },
+  random: () => {
+    return Math.random();
+  },
+  rollThreeDice: () => {
+    return [1, 2, 3].map(_ => 1 + Math.floor(Math.random() * 6));
+  },
+  rollDice: ({numDice, numSides}) => {
+    var output = [];
+    for (var i = 0; i < numDice; i++) {
+      output.push(1 + Math.floor(Math.random() * (numSides || 6)));
+    }
+    return output;
+  },
+  getDie: ({numSides}) => {
+    return new RandomDie(numSides || 6);
+  },
+  getMessage: ({id}) => {
+    if (!fakeDatabase[id]) {
+      throw new Error('no message exists with id ' + id);
+    }
+    return new Message(id, fakeDatabase[id]);
+  },
+  createMessage: ({input}) => {
+    // Create a random id for our "database".
+    var id = require('crypto').randomBytes(10).toString('hex');
+
+    fakeDatabase[id] = input;
+    return new Message(id, input);
+  },
+  updateMessage: ({id, input}) => {
+    if (!fakeDatabase[id]) {
+      throw new Error('no message exists with id ' + id);
+    }
+    // This replaces all old data, but some apps might want partial update.
+    fakeDatabase[id] = input;
+    return new Message(id, input);
   },
 };
 
@@ -20,7 +120,51 @@ const app = express();
 app.use('/graphql', graphqlHTTP({
   schema: schema,
   rootValue: root,
-  graphql: true,
+  graphiql: true,
 }));
 app.listen(4000);
 console.log('Running a GraphQL API server at http://localhost:4000/graphql');
+
+
+
+//in console at http://localhost:4000/graphql, can query any of the variables stored in root (our endpoints)
+
+// fetch('/graphql', {
+//   method: 'POST',
+//   headers: {
+//     'Content-Type': 'application/json',
+//     'Accept': 'application/json',
+//   },
+//   body: JSON.stringify({query: "{ hello }"})
+// })
+//   .then(r => r.json())
+//   .then(data => console.log('data returned:', data));
+
+
+
+
+
+
+
+//in console at http://localhost:4000/graphql
+
+// var dice = 3;
+// var sides = 6;
+// var query = `query RollDice($dice: Int!, $sides: Int) {
+//   rollDice(numDice: $dice, numSides: $sides)
+// }`;
+//
+// fetch('/graphql', {
+//   method: 'POST',
+//   headers: {
+//     'Content-Type': 'application/json',
+//     'Accept': 'application/json',
+//   },
+//   body: JSON.stringify({
+//     query,
+//     variables: { dice, sides },
+//   })
+// })
+//   .then(r => r.json())
+//   .then(data => console.log('data returned:', data));
+//https://graphql.org/graphql-js/passing-arguments/
